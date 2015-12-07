f741b65f6cba9bf3753ccaa1969491dbf98d679b
Mike Boeser <mboeser@yahoo.com>
2015-12-06 21:35:48 -0600

    add callback

--- server/config/auth.js
+++ server/config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : process.env.FACEBOOK_ID, // your App ID
         'clientSecret'    : process.env.FACEBOOK_SECRET, // your App Secret
-        'callbackURL'     : 'http://localhost:5000/auth/facebook/callback'
+        'callbackURL'     : process.env.PORT+'/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'        : process.env.TWITTER_KEY,
         'consumerSecret'     : process.env.TWITTER_SECERT,
-        'callbackURL'        : 'http://127.0.0.1:5000/auth/twitter/callback'
+        'callbackURL'        : process.env.PORT+'/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : process.env.GOOGLE_ID,
         'clientSecret'     : process.env.GOOGLE_SECRET,
-        'callbackURL'      : 'http://localhost:5000/auth/google/callback'
+        'callbackURL'      : process.env.PORT+'/auth/google/callback'
     }
 
 };
